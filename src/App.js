816572f4e01f36ba6204ba70f93e2fd8e0b1d266
kumar gaurav <gauravrajan101@gmail.com>
2022-12-07 15:36:33 +0530

    created board

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
+import Board from './components/Board'
 import './App.css';
 import React from 'react'
 
  const App=()=> {
   return (
     <>
-    <h1>This is React app</h1>
-      
+    
+      <Board/>
     </>
   )
 }
